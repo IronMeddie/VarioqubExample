f0fe45b198288223bbbf8da5d3209a9e4bf21fd8
taranyukm <mikaelofarius@gmail.com>
2023-08-15 10:25:07 +0300

    Varioqub

--- app/src/main/java/com/example/handlefirstactivationasupdate/ui/navigation/MainNavHost.kt
+++ app/src/main/java/com/example/handlefirstactivationasupdate/ui/navigation/MainNavHost.kt
@@ -13,6 +13,9 @@ import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.example.handlefirstactivationasupdate.ui.screens.main.MainScreen
 import com.example.handlefirstactivationasupdate.ui.screens.purchase.Purchase_A1
+import com.example.handlefirstactivationasupdate.ui.screens.purchase.Purchase_A2
+import com.example.handlefirstactivationasupdate.ui.screens.purchase.Purchase_B1
+import com.example.handlefirstactivationasupdate.ui.screens.purchase.Purchase_B2
 import com.yandex.varioqub.config.Varioqub
 
 @Composable
@@ -21,12 +24,19 @@ fun MainNavHost() {
 
     NavHost(navController = navController, startDestination = Screens.MainScreen) {
         composable(route = Screens.MainScreen) {
-            MainScreen(){
-                val flag = Varioqub.getString("PurchaseVariant", "first")  // получаем флаг какой из вариантов показывать
+            MainScreen() {
+                val flag = Varioqub.getString(
+                    "PurchaseVariant",
+                    "first"
+                )  // получаем флаг какой из вариантов показывать
                 Log.d("VARIOQUB flag buttonPosition ", flag)
-                when(flag){
-                    "first" -> {navController.navigateToPurchase1()} // переносим пользователя на нужный экран в зависимости от флага
-                    "second" -> {navController.navigateToPurchase2()}
+                when (flag) {
+                    "first" -> {
+                        navController.navigateToPurchase1()
+                    } // переносим пользователя на нужный экран в зависимости от флага
+                    "second" -> {
+                        navController.navigateToPurchase2()
+                    }
                 }
             }
         }
@@ -39,27 +49,33 @@ fun MainNavHost() {
         }
 
 
-        navigation(startDestination = Screens.PurchaseA1, route = Screens.PurchaseScreen1) {// есть две вложенные навигации по экранам оплаты
+        navigation(
+            startDestination = Screens.PurchaseA1,
+            route = Screens.PurchaseScreen1
+        ) {// есть две вложенные навигации по экранам оплаты
             composable(route = Screens.PurchaseA1) {                                         // будет показываться одна из них в зависимости от полученого флага Varioqub
                 Purchase_A1 {
                     navController.toA2()
                 }
             }
             composable(route = Screens.PurchaseA2) {
-                Purchase_A1 {
+                Purchase_A2 {
                     navController.navigateToMainScreen()
                 }
             }
 
         }
-        navigation(startDestination = Screens.PurchaseB1, route = Screens.PurchaseScreen2) {// есть две вложенные навигации по экранам оплаты
+        navigation(
+            startDestination = Screens.PurchaseB1,
+            route = Screens.PurchaseScreen2
+        ) {// есть две вложенные навигации по экранам оплаты
             composable(route = Screens.PurchaseA1) {
-                Purchase_A1 {
+                Purchase_B1 {
                     navController.toB2()
                 }
             }
             composable(route = Screens.PurchaseB2) {
-                Purchase_A1 {
+                Purchase_B2 {
                     navController.navigateToMainScreen()
                 }
             }
